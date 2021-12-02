71013407ee6d1eb4a429aed2d651206819856c4d
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-12-02 19:35:16 -0300

    Add files via upload

---
+++ Exerc_13/Exerc_13/Calculo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_13
+{
+    class Calculo
+    {
+        #region atributos
+        private double num1;
+        private double num2;
+        private double num3;
+        private double resultado;
+        private string resultadofinal;
+        #endregion
+
+        #region construtores
+        public Calculo()
+        {
+            this.num1 = 0;
+            this.num2 = 0;
+            this.num3 = 0;
+        }
+
+        public Calculo(double num1, double num2, double num3)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+        #endregion
+
+        #region métodos de interface (Setter's & Getter's)
+        public void setNum1(double num1)
+        {
+            this.num1 = num1;
+        }
+
+        public void setNum2(double num2)
+        {
+            this.num2 = num2;
+        }
+
+        public void setNum3(double num3)
+        {
+            this.num3 = num3;
+        }
+
+        public double getNum1()
+        {
+            return this.num1;
+        }
+
+        public double getNum2()
+        {
+            return this.num2;
+        }
+        public double getNum3()
+        {
+            return this.num3;
+        }
+        public double getResultado()
+        {
+            return this.resultado;
+        }
+        public string getResultadofinal()
+        {
+            return this.resultadofinal;
+        }
+        #endregion
+
+        #region métodos funcionais
+        public void calcular()
+        {
+            this.resultado = (this.num2 * this.num2) + (this.num3 * this.num3);
+
+            if (this.resultado == this.num1 * this.num1)
+            {
+                this.resultadofinal = "Os valores inseridos são de um triângulo retângulo";
+            }
+
+            else
+            {
+                this.resultadofinal = "Os valores inseridos não são de um triângulo retângulo";
+            }
+        }
+        #endregion
+    }
+}
---
+++ Exerc_13/Exerc_13/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exerc_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Calculo s;
+            s = new Calculo();
+            s.setNum1(double.Parse(txtNum1.Text));
+            s.setNum2(double.Parse(txtNum2.Text));
+            s.setNum3(double.Parse(txtNum3.Text));
+            s.calcular();
+            lblResultado.Text = s.getResultadofinal();
+        }
+    }
+}
